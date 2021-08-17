34c2204d91f4da1943527969d03f0343520ed256
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-17 10:23:13 +0100

    pads repeat animation

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumKit.playBtn.addEventListener("click", function () {
